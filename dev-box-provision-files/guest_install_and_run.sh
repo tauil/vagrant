770c3502e7371fd28464e2f431c3983f8ebd04b8
Rafael B. Tauil <rafael@tauil.com.br>
2016-08-23 20:26:18 +0000

    Adds postgres and mysql provision files for Ubuntu 14.04

---
+++ dev-box-provision-files/guest_install_and_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+if [[ ! -x /usr/bin/ansible ]]; then
+  echo 'Installing ansible...'
+  apt-add-repository -y ppa:rquillo/ansible
+  apt-get update
+  apt-get install -y ansible
+  echo '[local]' > /etc/ansible/hosts
+  echo 'localhost' >> /etc/ansible/hosts
+  echo 'Ansible installed.'
+else
+  echo 'Ansible already installed.'
+fi
+
+echo 'Running playbook...'
+cd /projects
+ansible-playbook dev-box-provision-files/playbook.yml
+echo 'Playbook finished.'
